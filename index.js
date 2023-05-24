0689b961e5a06bc38d0dbbe5142e6bed0815b82e
Daniel Landeros <133705352+Undanielx@users.noreply.github.com>
2023-05-23 22:11:57 -0600

    routing con express

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const port = 3000;
 
 app.get('/', (req, res)=> {
  res.send('Hola mi server en express');
-})
+});
+
+app.get('/nueva-ruta', (req, res)=> {
+  res.send('Hola, soy la nueva ruta');
+ });
+
+ app.get('/products', (req, res)=> {
+  res.json({
+    name: 'producto 1',
+    price: 1000
+  });
+ });
 
 app.listen(port, ()=> {
   console.log('Mi port' + port);

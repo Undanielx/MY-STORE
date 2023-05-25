382a65ae5b68f4e9cc46dc34d1e55b9c3307929e
Daniel Landeros <133705352+Undanielx@users.noreply.github.com>
2023-05-24 22:25:05 -0600

    Get_Parametros Query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
@@ -11,17 +12,23 @@ app.get('/nueva-ruta', (req, res)=> {
  });
 
  app.get('/products', (req, res)=> {
-  res.json([
-  {
-    name: 'producto 1',
-    price: 1000
-  } ,
-  {
-    name: 'producto 2',
-    price: 2000
+  const products =[];
+  const { size } = req.query;
+  const limit =size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name:faker.commerce.productName(),
+      price:parseInt(faker.commerce.price(), 10),
+      Image: faker.image.imageUrl()
+    })
+
   }
-  ]);
+  res.json(products)
  });
+
+ app.get('/products/filter',(req, res)=>{
+  res.send('Yo soy un filtro');
+})
 
  app.get('/products/:id', (req, res)=>{
   const {id}= req.params;
@@ -29,7 +36,19 @@ app.get('/nueva-ruta', (req, res)=> {
     id,
     name: 'producto 2',
     price: 2000
-  })
+  });
+ });
+
+ app.get('/users',(req, res)=>{
+  const {limit, offset}= req.query;
+  if(limit && offset){
+    res.json({
+       limit,
+       offset
+    });
+  }else{
+    res.send('No hay parametros');
+  }
  })
 
  app.get('/categories/:categoryId/products/:productId', (req, res)=>{
@@ -38,7 +57,7 @@ app.get('/nueva-ruta', (req, res)=> {
     categoryId,
     productId
   });
- })
+ });
 
 app.listen(port, ()=> {
   console.log('Mi port' + port);

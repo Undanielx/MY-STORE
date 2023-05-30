05b64a71c9836b3e5ba8b821e005e6406601cfef
Daniel Landeros <133705352+Undanielx@users.noreply.github.com>
2023-05-30 11:24:14 -0600

    Codigos de estado o HTTP response status code

--- routes/products.router.js
+++ routes/products.router.js
@@ -24,16 +24,23 @@ router.get('/', (req, res)=> {
 
 router.get('/:id', (req, res)=>{
   const {id}= req.params;
-  res.json({
-    id,
-    name: 'producto 2',
-    price: 2000
-  });
+  if (id === '999')
+  {
+    res.status(404).json({
+      message :'not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'producto 2',
+      price: 2000
+    });
+  }
  });
 
 router.post('/',(req , res)=>{
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message : 'created',
     data : body
   })
